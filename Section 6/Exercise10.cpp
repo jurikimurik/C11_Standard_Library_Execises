f591e3af319735a81d55178150c3a205eb9e6299
jurikimurik <makovskyiyurii@gmail.com>
2023-02-06 13:44:41 +0100

    Added completed Exercises10,11

--- Section 6/Exercise10.cpp
+++ Section 6/Exercise10.cpp
@@ -1,2 +1,25 @@
 // Zadanie 10 - usuwacz. Posiadasz vector<char> z zawartoscia {'S','A','b','S','S','b','S','y','S'}. Usun z niego za pomoca algorytmow litere 'S' i niech vector zmieni swoj rozmiar na prawidlowy.
 // Wypisz odpowiedz na ekranie.
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//************************************************************************************************************************
+int main()
+{
+    vector<char> litery = {'S',
+                           'A',
+                           'b',
+                           'S',
+                           'S',
+                           'b',
+                           'S',
+                           'y',
+                           'S'};
+
+    litery.erase(remove(litery.begin(), litery.end(), 'S'), litery.end());
+
+    for(const auto& elem : litery) {
+        cout << elem;
+    }
+}
--- Section 6/Exercise11.cpp
+++ Section 6/Exercise11.cpp
@@ -1,2 +1,15 @@
 // Zadanie 11. Usuwanie z kontenerow alokacyjnych i nieporzadkujacych. Masz podany kontener set<int> z wartosciami {2,3,5,1,2,3,4,3,1,2,2,3,4,2}. Usun z niego wszystkie dwojki za pomoca dowolnego sposobu.
- 
+ #include <unordered_set>
+ #include <algorithm>
+ #include <iostream>
+using namespace std;
+//************************************************************************************************************************
+int main()
+{
+    unordered_set<int> liczby = {2, 3, 5, 1, 2, 3, 4, 3, 1, 2, 2, 3, 4, 2};
+    liczby.erase(2);
+    for(const auto& elem: liczby) {
+        cout << elem << ", ";
+    }
+    cout << endl;
+}
