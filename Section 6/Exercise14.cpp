1147f066a0eb040f6d67d41783a9d28179b859ac
jurikimurik <makovskyiyurii@gmail.com>
2023-02-06 14:43:41 +0100

    Added Exercise14, 15

--- Section 6/Exercise14.cpp
+++ Section 6/Exercise14.cpp
@@ -1,2 +1,44 @@
 // Zadanie 14 - Wielkie Sprzatanie. Posiadasz koneter deque<string>, ktory miesci w sobie wiele mebli dostepnych w pokoju. Jednak dodaj w losowe miejsca tego pokoju "Karaluch". Wypisz powstaly kontener na
 // ekranie a nastepnie "zadzwon po inspektora" i usun wszystkich karaluchow. (Skorzystaj z algorytmow i lambdy do okreslenia czy podany obiekt jest karaluchiem).
+#include <deque>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//********************************************************************************************************************************************************************
+deque<string> pokoj = {"Lozko", "Szafa", "Stol", "Krzeslo", "Monitor", "Okno", "Telewizja", "Wielkie krzeslo", "Maly stolik", "Polki"};
+//********************************************************************************************************************************************************************
+int main()
+{
+    srand(time(NULL));
+    for (int i = 0, los = rand() % pokoj.size(); i < los; i++) {
+        auto iter = pokoj.begin() + rand() % pokoj.size();
+        pokoj.insert(iter, "Karaluch");
+    }
+
+    cout << "Zawartosc pokoju przed sprzataniem: ";
+    for (const auto &elem : pokoj)
+    {
+        cout << elem << ", ";
+    }
+    cout << endl;
+
+    cout << "\nDzwonimy po inspektora!" << endl;
+
+    pokoj.erase(remove_if(pokoj.begin(), pokoj.end(), [](string cos)
+              {
+        if(cos == "Karaluch") {
+            return true;
+        } else {
+            return false;
+        } }), pokoj.end());
+
+        
+    cout << endl;
+
+    cout << "Zawartosc pokoju po sprzataniu: ";
+    for (const auto &elem : pokoj)
+    {
+        cout << elem << ", ";
+    }
+    cout << endl;
+}
--- Section 6/Exercise15.cpp
+++ Section 6/Exercise15.cpp
@@ -1,1 +1,18 @@
 // Zadanie 15 - Dwojki i jedynki. Korzystajac z lambdy w algorymie sortujacym, posortuj wszystko tak, by liczby podzielne przez 2 byly na samym poczatku, a inne - na samym koncu.
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//***************************************************************************************************************************************************************************************
+int main()
+{
+    vector<int> liczby = {1,2,3,4,5,6};
+
+    sort(liczby.begin(), liczby.end(), [](int a, int b)
+         { return (a % 2 == 0); });
+
+    for(auto & elem : liczby) {
+        cout << elem << ", ";
+    }
+    cout << endl;
+}
