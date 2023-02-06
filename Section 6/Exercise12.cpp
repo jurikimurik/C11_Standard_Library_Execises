88433b67a153b9ab0577914130ffd406f058ed12
jurikimurik <makovskyiyurii@gmail.com>
2023-02-06 14:28:29 +0100

    Added completed Exercise12,13

--- Section 6/Exercise12.cpp
+++ Section 6/Exercise12.cpp
@@ -1,1 +1,58 @@
 // Zadanie 12 - Ubrania. Masz konetener osob typu deque<string>, gdzie kazdy z nich ma przydomek "Nagi". Za pomocą algorytmu i przekazanej funkcji, zmien ten przydomek u kazdej osoby na "Ubrany".
+#include <deque>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//*******************************************************************************************************************************************************************************
+
+deque<string> osoby = {"Nagi Janusz",
+                       "Nagi Dobromił",
+                       "Nagi Janusz",
+                       "Nagi Ignacy",
+                       "Nagi Korneliusz",
+                       "Nagi Ernest",
+                       "Nagi Bruno",
+                       "Nagi Eustachy",
+                       "Nagi Paweł",
+                       "Nagi Klaudiusz",
+                       "Nagi Dorian",
+                       "Nagi Leonardo",
+                       "Nagi Kordian",
+                       "Nagi Remigiusz",
+                       "Nagi Robert",
+                       "Nagi Kajetan",
+                       "Nagi Natan",
+                       "Nagi Miłosz",
+                       "Nagi Jerzy",
+                       "Nagi Gustaw"};
+
+//*******************************************************************************************************************************************************************************
+
+string kwatermistrz(string osoba) {
+    string slowo = "Nagi ";
+    string::iterator pos = osoba.begin();
+    pos += osoba.find(slowo);
+    osoba.erase(pos, pos + slowo.length());
+
+    osoba = string("Ubrany ") + osoba;
+    return osoba;
+}
+
+int main()
+{
+    for(const auto& elem : osoby) {
+        cout << elem << ", ";
+    }
+    cout << endl;
+
+    cout << endl
+         << "Ruszamy do KWATERMISTRZA!" << endl;
+
+    transform(osoby.begin(), osoby.end(), osoby.begin(), kwatermistrz);
+
+    for (const auto &elem : osoby)
+    {
+        cout << elem << ", ";
+    }
+    cout << endl;
+}
--- Section 6/Exercise13.cpp
+++ Section 6/Exercise13.cpp
@@ -1,2 +1,70 @@
 // Zadanie 13 - Przystepcy w pociagu. Masz vector z imonami i nazwiskami osob, ktorzy jada tym pociagiem. Rowniez posiadasz spis przestepcow. Przeszukaj caly pociag i wyrzuc kazdego przestepce z pociagu
 // oznajmniajac to wielkim komunikatem.
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//************************************************************************************************************************************************************************************
+
+vector<string> pasazerowie = {"Kajetan Kubiak",
+                              "Andrzej Mazur",
+                              "Olaf Pawlak",
+                              "Boleslaw Szymczak",
+                              "Cyprian Krupa",
+                              "Ireneusz Pawlak",
+                              "Ignacy Jakubowski",
+                              "Olaf Sikorska",
+                              "Mariusz Brzezinski",
+                              "Ireneusz Makowski",
+                              "Ignacy Maciejewski",
+                              "Amir Lewandowski",
+                              "Marcin Wisniewski",
+                              "Bartosz Chmielewski",
+                              "Heronim Gajewska",
+                              "Lukasz Piotrowski",
+                              "Mikolaj Czarnecki",
+                              "Boleslaw Szczepanski",
+                              "Gabriel Malinowski",
+                              "Edward Dabrowski"};
+
+vector<string> przestepcy = {"Heronim Gajewska", "Boleslaw Szymczak", "Olaf Sikorska", "Amir Lewandowski"};
+
+//************************************************************************************************************************************************************************************
+
+void wyrzuc(vector<string>& pasazery, vector<string>::iterator pos) {
+    cout << "Wyrzucamy z pociagu " << *pos << "! Zegnaj, zlodzieju!" << endl;
+
+    pasazery.erase(pos);
+}
+
+bool czyPrzystepca(string osoba) {
+    auto pos = find(przestepcy.begin(), przestepcy.end(), osoba);
+    if (pos != przestepcy.cend())
+    {
+        cout << "Przestepca znaleziony!" << endl;
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    cout << "Przed przeszukiwaniem: \n";
+    for(auto &elem : pasazerowie) {
+        cout << "\t" << elem << endl;
+    }
+    cout << endl;
+
+    auto pos = find_if(pasazerowie.begin(), pasazerowie.end(), czyPrzystepca);
+    while (pos != pasazerowie.end())
+    {
+        wyrzuc(pasazerowie, pos);
+        pos = find_if(pasazerowie.begin(), pasazerowie.end(), czyPrzystepca);
+    }
+
+    cout << "Po przeszukaniu: \n";
+    for (auto &elem : pasazerowie)
+    {
+        cout << "\t" << elem << endl;
+    }
+}
