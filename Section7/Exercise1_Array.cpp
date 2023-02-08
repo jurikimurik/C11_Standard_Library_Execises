5277e2094bce29d23aac6921f6c0f67b221c093c
jurikimurik <makovskyiyurii@gmail.com>
2023-02-08 13:24:59 +0100

    Added DONE exercise1, 3, 5

--- Section7/Exercise1_Array.cpp
+++ Section7/Exercise1_Array.cpp
@@ -1,2 +1,70 @@
 // Zadanie 1 - Tablica zmian. Masz podany kontener array<int, 5> z wartosciami {1, 2, 3, 4, 5}. Spytaj sie uzytkownika, czy chce zmienic tam jakies elementy. Jezeli tak, to pozwol mu na to, lub zakoncz
 // wykonywanie programu. Przetestuj dzialanie programu jezeli wprowadzisz za duzo liczb rowniez.
+#include <array>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//********************************************************************************************************************************************
+array<int, 5> zbior = {1, 2, 3, 4, 5};
+//********************************************************************************************************************************************
+void wypisz(array<int, 5>& zb) {
+    for(const auto& elem : zb) {
+        cout << elem << ", ";
+    }
+    cout << endl;
+}
+//********************************************************************************************************************************************
+void zmiana(array<int, 5>& zb) {
+    cout << "Podaj indeks 1: ";
+    int index1;
+    cin >> index1;
+    index1--;
+    if (index1 < 0 || index1 > zb.size())
+    {
+        cout << "Zly index. Biore 0." << endl;
+        index1 = 0;
+    }
+
+    cout << "Podaj indeks 2: ";
+    int index2;
+    cin >> index2;
+    index2--;
+    if (index2 < 0 || index2 > zb.size())
+    {
+        cout << "Zly index. Biore 0." << endl;
+        index2 = 0;
+    }
+
+    if(index2-index1 >= zb.size()) {
+        cout << "Zle podane indeksy. Wracam." << endl;
+        return;
+    }
+
+    while (index2 - index1 >= 0)
+    {
+        cout << "Podaj liczbe:";
+        int liczba;
+        cin >> liczba;
+        zb.at(index1++) = liczba;
+    }
+}
+//********************************************************************************************************************************************
+int main()
+{
+    while (true)
+    {
+        wypisz(zbior);
+        cout << "Chcesz wprowadzic zmiany? 1 - Tak, 2 - Nie" << endl;
+        int odp;
+        cin >> odp;
+        switch (odp)
+        {
+        case 1:
+            zmiana(zbior);
+            break;
+
+        default:
+            return 0;
+        }
+    }
+}
--- Section7/Exercise3_Vector.cpp
+++ Section7/Exercise3_Vector.cpp
@@ -1,3 +1,39 @@
-// Zadanie 3 - Sprawdzenie wydajnosci. Stworz vector<int> z losowymi liczbami o ilosci 1000. Nastepnie stworz kopię tego vectora. Od tego momentu zacznij zmierzac czas dla jednego przypadku: Wstaw
+// Zadanie 3 - Sprawdzenie wydajnosci. Stworz vector<int> z losowymi liczbami o ilosci 100000. Nastepnie stworz kopię tego vectora. Od tego momentu zacznij zmierzac czas dla jednego przypadku: Wstaw
 // wszystkie elementy drugiego wektora do wektora pierwszego za pomoca push_back i wypisz czas. Nastepnie przywroc stary vector do poczatkowego rozmiaru i zacznij liczyc czas ponownie. Tym razem
 // jednak wstaw elementy z drugiego wektora do srodka vectoru pierwszego i wypisz czas.
+#include <vector>
+#include <chrono>
+#include <iostream>
+using namespace std;
+//*******************************************************************************************************************************************************************************
+int main()
+{
+    srand(time(NULL));
+    vector<int> wektor1;
+    for (int i = 0; i < 100000; ++i) {
+        wektor1.push_back(rand() % 1000);
+    }
+
+    vector<int> wektor2(wektor1);
+
+    auto czas = chrono::system_clock::now();
+    for (int i = 0; i < wektor2.size(); ++i) {
+        wektor1.push_back(wektor2[i]);
+    }
+
+    cout << "Czas 1 (w milisekundach): " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - czas).count() << endl;
+
+    wektor1.resize(100000);
+    czas = chrono::system_clock::now();
+
+    for (int i = 0; i < wektor2.size(); ++i)
+    {
+        wektor1.insert(wektor1.begin() + (wektor1.size() / 2), wektor2[i]);
+    }
+
+    //NAJLEPSZA METODA:
+    //wektor1.insert(wektor1.begin() + (wektor1.size() / 2), wektor2.cbegin(), wektor2.cend());
+
+    cout << "Czas 2 (w milisekudnach): " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - czas).count() << endl;
+
+}
