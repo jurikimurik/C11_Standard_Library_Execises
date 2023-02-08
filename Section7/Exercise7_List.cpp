44c52ce720351f7558bd944bfb8d9ddf799b584d
jurikimurik <makovskyiyurii@gmail.com>
2023-02-08 15:07:58 +0100

    Added completed exercises7, 8.

--- Section7/Exercise7_List.cpp
+++ Section7/Exercise7_List.cpp
@@ -1,2 +1,36 @@
-// Zadanie 7 - Sprawdzenie wydajnosci. Stworz vector<int> i list z liczbami od 1 do 2000. Nastepnie do kazdej z tych list w srodek dodaj liczby od 2001 do 4000. Oblicz z jaka szybkoscia robi to vector i 
-// z jaka list. Prownaj te dwa wyniki
+// Zadanie 7 - Sprawdzenie wydajnosci. Stworz vector<int> i list z liczbami od 0 do 200000. Nastepnie do kazdej z tych kontenerow na poczatek dodaj liczby od 2001 do 4000. Oblicz z jaka szybkoscia robi to 
+//vector i z jaka list. Porownaj te dwa wyniki.
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <chrono>
+#include <iostream>
+using namespace std;
+//********************************************************************************************************************************************************************************
+int main()
+{
+    vector<int> wektor;
+    list<int> lista;
+
+    int liczba = 200000;
+    for (int i = 0; i < liczba; ++i) {
+        wektor.push_back(i);
+        lista.push_back(i);
+    }
+
+    auto czas = chrono::system_clock::now();
+
+   
+    for (int i = 0; i < liczba; ++i)
+    {
+        wektor.insert(wektor.begin(), i);
+    }
+    cout << "Dla wektora: " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - czas).count() << endl;
+
+    czas = chrono::system_clock::now();
+    for (int i = 0; i < liczba; ++i)
+    {
+        lista.insert(lista.begin(), i);
+    }
+    cout << "Dla listy: " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - czas).count() << endl;
+}
--- Section7/Exercise8_List.cpp
+++ Section7/Exercise8_List.cpp
@@ -1,2 +1,54 @@
 // Zadanie 8 - Symbioza. Posiadasz dwie listy z losowymi liczbami o ilosci 10. Zrob tak, zeby polowa pierwszej listy znalazla sie w drugiej i odwrotnie. Nastpenie posortuj kazda z tych list i zrob
 // unikatowa. Wynik wypisz na ekranie.
+#include <list>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//********************************************************************************************************************************************************************************
+void wypiszListy(list<int> p, list<int> d, string text = "") {
+    cout << text << endl;
+    cout << "\tlista 1:";
+    for(const auto& elem : p)
+    {
+        cout << elem << ", ";
+    }
+    cout << endl
+         << endl
+         << "\tlista 2:";
+    for(const auto& elem : d) {
+        cout << elem << ", ";
+    }
+    cout << endl
+         << endl;
+}
+//********************************************************************************************************************************************************************************
+int main()
+{
+    list<int> pierwsza;
+    list<int> druga;
+
+    srand(time(NULL));
+    for (int i = 0; i < 10; ++i) {
+        pierwsza.push_back(rand() % 20);
+        druga.push_back(rand() % 20);
+    }
+
+    wypiszListy(pierwsza, druga, "Przed: ");
+
+    auto pos1 = pierwsza.begin();
+    for (int i = 0; i < pierwsza.size() / 2; ++i)
+    {
+        pos1++;
+    }
+
+    swap_ranges(pierwsza.begin(), pos1, druga.begin());
+    wypiszListy(pierwsza, druga, "Po swap: ");
+
+    pierwsza.sort();
+    pierwsza.unique();
+
+    druga.sort();
+    druga.unique();
+
+    wypiszListy(pierwsza, druga, "Po sortowaniu i unikatowosci : ");
+}
