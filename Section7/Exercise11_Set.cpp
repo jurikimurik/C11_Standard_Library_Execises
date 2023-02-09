ea02a87eb9df382b148dc2e7495e860ed1466e6d
jurikimurik <makovskyiyurii@gmail.com>
2023-02-09 16:35:12 +0100

    Added completed Exercises11, 12.

--- Section7/Exercise11_Set.cpp
+++ Section7/Exercise11_Set.cpp
@@ -1,1 +1,42 @@
 // Zadanie 11 - Porownywanie wyszukiwania. Stworz vector<int> oraz set<int> z liczbami od 0 do 2000000. Wyszukaj w kazdym z nich okresloną liczbe i porwnaj czasowe wyniki dla vector oraz dla set.
+#include <vector>
+#include <set>
+#include <chrono>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    vector<int> wektor;
+    set<int> zbior;
+
+    int ilosc_liczb = 20000000;
+
+    for (int i = 0; i < ilosc_liczb; ++i)
+    {
+        wektor.push_back(i);
+        zbior.insert(i);
+    }
+
+    cout << "Podaj liczbe z zakresu 0 do 20000000:";
+    int liczba;
+    cin >> liczba;
+
+    auto czas = chrono::system_clock::now();
+
+    if(find(wektor.begin(), wektor.end(), liczba) != wektor.end()) {
+        cout << "Element znaleziony!" << endl;
+    }
+
+    cout << "Czas dla wektoru: " << chrono::duration_cast<chrono::nanoseconds>(chrono::system_clock::now() - czas).count() << endl;
+
+    czas = chrono::system_clock::now();
+
+    if (zbior.find(liczba) != zbior.end())
+    {
+        cout << "Element znaleziony!" << endl;
+    }
+
+    cout << "Czas dla set'u: " << chrono::duration_cast<chrono::nanoseconds>(chrono::system_clock::now() - czas).count() << endl;
+}
--- Section7/Exercise12_Set.cpp
+++ Section7/Exercise12_Set.cpp
@@ -1,3 +1,58 @@
 // Zadanie 12 - Modulo 2. Stworz szablon, ktory bedzie typem kryterium sortowania. Zdefinuj w nim takie zachowanie, zeby na poczatku mial liczby podzielne przez 2, a na koncu nie. Udostepnij tryb pracy
 // odwrotnej rowniez. Nastepnie stworz dwa kontenery set z roznymi ustawieniami trybu sortowania, ale wspolnym typem sortowania (stworzonym przez ciebie). Uzupelnij ich liczbami i sprawdz czy dobrze
 // sortuja. Nastepnie przypisz do pierwszego konteneru drugi i sprawdz, co się stało, wypisując na ekranie rezultat.
+#include <set>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class Sortownik {
+public:
+
+    enum tryb
+    {
+        normalny,
+        odwrotny
+    };
+
+private: 
+    tryb wybor;
+
+public:
+    Sortownik(tryb jaki_tryb = normalny) : wybor(jaki_tryb){}
+
+    bool operator () (const int& l1, const int& l2) const {
+        return wybor == normalny ? l1 % 2 == 0 || l2 % 2 == 1 : l2 % 2 == 0 || l1 % 2 == 1;
+    }
+};
+
+int main()
+{
+    set<int, Sortownik> liczby1 = {1, 5, 4, 3, 7, 6, 8, 2, 9, 10};
+
+    Sortownik reverse_order(Sortownik::odwrotny);
+    set<int, Sortownik> liczby2(reverse_order);
+    liczby2 = {1, 5, 4, 3, 7, 6, 8, 2, 9, 10};
+
+    cout << "Liczby 1: ";
+    for (const auto &elem : liczby1)
+    {
+        cout << elem << ", ";
+    }
+    cout << endl;
+
+    cout << "Liczby 2: ";
+    for (const auto &elem : liczby2)
+    {
+        cout << elem << ", ";
+    }
+    cout << endl;
+
+    liczby1 = liczby2;
+    cout << "Po przypisaniu - liczby 1: ";
+    for (const auto &elem : liczby1)
+    {
+        cout << elem << ", ";
+    }
+    cout << endl;
+}
