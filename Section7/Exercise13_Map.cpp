3e66f7f58f6698480e199ee34021f1bb5260f541
jurikimurik <makovskyiyurii@gmail.com>
2023-02-09 16:59:38 +0100

    Added completed 13, 14 about maps.

--- Section7/Exercise13_Map.cpp
+++ Section7/Exercise13_Map.cpp
@@ -1,2 +1,35 @@
 // Zadanie 13 - Ksiazka telefoniczna. Posiadasz okreslony zbior danych map<string, int>, w ktorym string okresla imie przyjaciela(ki) a int - jego numer telefonu bez (+48). Niech uzytkownik wprowadza
 // imiona osob, ktorych numer telefonu potrzebuje, a zwrotnie dostaje informacje dotyczaca tego, jaki ten przyjaciel(ka) ma numer telefonu.
+#include <map>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+//************************************************************************************************************************************************************************
+map<string, int> ksiazka_telefoniczna = {{"Piotr", 545234523},
+                                         {"Damian", 242533435},
+                                         {"Pawel", 235334353},
+                                         {"Kornel", 234898098},
+                                         {"Olaf", 245332445},
+                                         {"Eustachy", 345563234},
+                                         {"Oskar", 345609454},
+                                         {"Aureliusz", 335345231},
+                                         {"Daniel", 134523533}};
+//************************************************************************************************************************************************************************
+int main()
+{
+    while(true) {
+        cout << "Wprowadz imie: ";
+        string imie;
+        cin >> imie;
+
+        if(imie == "q")
+            return 0;
+
+        if (auto pos = ksiazka_telefoniczna.find(imie); pos != ksiazka_telefoniczna.end())
+        {
+            cout << "Numer telefonu: +48" << pos->second << endl;
+        } else {
+            cout << "Nie znaleziono. Sprobuj ponownie lub wyjdz \'q\'" << endl;
+        }
+    }
+}
--- Section7/Exercise14_Map.cpp
+++ Section7/Exercise14_Map.cpp
@@ -1,2 +1,45 @@
-// Zadanie 14 - Słownik. Stworz słownik, do ktorego uzytkonik bedzie mogl wprowadzac slowa a na koniec wypisze mu wszystkie wprowadzone przez niego słowa w postaci posortowanej od litery A do Z lub 
+// Zadanie 14 - Słownik. Stworz słownik, do ktorego uzytkownik bedzie mogl wprowadzac slowa a na koniec wypisze mu wszystkie wprowadzone przez niego słowa w postaci posortowanej od litery A do Z lub 
 // odwrotnie, jezeli bedzie tego chcial.
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+//*******************************************************************************************************************************************************************************
+int main()
+{
+    multimap<char, string> slownik;
+
+    cout << "Wprowadzaj slowo po kolei i koncz przyciskiem ENTER. Gdy skonczysz, wpisz litere q" << endl;
+    while (true)
+    {
+        string slowo;
+        cin >> slowo;
+
+        if(slowo == "q")
+            break;
+
+        slownik.insert(make_pair(toupper(slowo.at(0)), slowo));
+    }
+
+    cout << "Sortowanie od A do Z - 1, Sortowanie od Z do A - 2" << endl;
+    int liczba;
+    cin >> liczba;
+    if(liczba == 2)
+    {
+        multimap<char, string, greater<char>> slownik_odwrotny(slownik.begin(), slownik.end());
+        for (const auto &elem : slownik_odwrotny)
+        {
+            cout << elem.first << " - " << elem.second << endl;
+        }
+        cout << endl;
+    }
+    else
+    {
+        for (const auto &elem : slownik)
+        {
+            cout << elem.first << " - " << elem.second << endl;
+        }
+        cout << endl;
+    }
+}
