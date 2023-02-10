37ed7fe4eb31f555cc77d0bd82bb906cd435e753
jurikimurik <makovskyiyurii@gmail.com>
2023-02-10 13:25:54 +0100

    Added 3 huge exercises connected with STL cont.

---
+++ Section 8/Exercise2.cpp
@@ -0,0 +1,3 @@
+// Zadanie 2 - Drzewo binarne z polecenia. Niech uzytkownik wprowadzi najpierw tryb: postfix / prefix. Nastepnie niech wprowadzi okreslone
+// symbole az do znaku 'q', wtedy jest koniec wprowadzania. Niech program zgeneruje wizualne drzewo binarne na podstawie wprowadzonych
+// przez uzytkownika danych.
