9fcfdc013c732d7729e3752146fc63311aa06363
jurikimurik <makovskyiyurii@gmail.com>
2023-02-15 14:40:45 +0100

    Done main functionality in Exercise2.

--- Practice/Exercise2.cpp
+++ Practice/Exercise2.cpp
@@ -1,2 +1,102 @@
 // Zadanie 2 - Znajdywanka. Masz plik danych z slowami. Zadaniem uztkownika jest w okreslonej ilosci rund podac na ktorym miejscu znajduje sie podana litera i zaliczyc poziom
 // (dostajes punkty) lub nie dostajesz nic. Na koncu wypisz wynik.
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <iterator>
+#include <fstream>
+using namespace std;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+using jednostka = string;
+using kontener = list<jednostka>;
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+kontener zbior;
+int punkty = 0;
+//***************************************************************************************************************************************************************************
+template <typename T>
+auto wprowadzenie(string napis = "")
+{
+    cout << napis;
+    T zmienna;
+    cin >> zmienna;
+    return zmienna;
+}
+//***************************************************************************************************************************************************************************
+void wypisz_zbior(kontener zb = zbior)
+{
+    for(const auto& elem : zb)
+    {
+        cout << elem << " - ";
+    }
+    cout << endl;
+}
+//***************************************************************************************************************************************************************************
+bool wgraj_dane(string nazwa)
+{
+    nazwa += ".tekst";
+
+    ifstream strm (nazwa);
+    if(!strm)
+    {
+        cout << "Nie udalo sie zaladowac, sprobuj ponownie" << endl;
+        return false;
+    }
+
+    copy(istream_iterator<string>(strm), istream_iterator<string>(), inserter(zbior, zbior.begin()));
+
+    return true;
+}
+//***************************************************************************************************************************************************************************
+bool runda(int ilosc_rund, kontener zb = zbior)
+{
+    srand(time(NULL));
+    
+    cout << "\tRunda " << ilosc_rund << "!" << endl;
+
+    //slowo
+    int losowosc = rand() % zb.size();
+    auto pos = zb.begin();
+    advance(pos, losowosc);
+    string slowo = *pos;
+
+    //litera
+    losowosc = rand() % slowo.size();
+    char znak = slowo.at(losowosc);
+
+    cout << slowo << " - podaj na ktorym miejscu jest pierwsza litera \'" << znak << "\': ";
+    int odpowiedz = wprowadzenie<int>();
+
+    if(odpowiedz == losowosc + 1)
+    {
+        cout << "Dobrze!" << endl;
+        return true;
+    } else {
+        cout << "Zle!" << endl;
+        return false;
+    }
+
+
+}
+//***************************************************************************************************************************************************************************
+void wynik(int punktacja = punkty)
+{
+    cout << "Zdobyles " << punktacja << " punktow! Brawo!" << endl;
+}
+//***************************************************************************************************************************************************************************
+int main()
+{
+    while(!wgraj_dane(wprowadzenie<string>("Wpisz nazwe pliku: ")));
+
+    wypisz_zbior();
+
+    size_t ilosc_rund = wprowadzenie<size_t>("Wpisz ilosc rund: ");
+
+    while(ilosc_rund > 0)
+    {
+        if(runda(ilosc_rund, zbior))
+            punkty += 100;
+        ilosc_rund--;
+    }
+
+    wynik(punkty);
+}
