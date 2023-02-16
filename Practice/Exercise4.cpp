7e6d01fb561c60d789f0022729fe090f912bf8d9
jurikimurik <makovskyiyurii@gmail.com>
2023-02-16 12:49:59 +0100

    Created an Exercise4.

---
+++ Practice/Exercise4.cpp
@@ -0,0 +1,2 @@
+//Zadanie 4 - Lista dzialan. Niech uzytkownik wprowadzi liste zadan na dzis. Nastepnie bedzie mogl ja edytowac: dodawac, usuwac, zmieniac, zamieniac miejscami, zapisywac i 
+//odczytywac.
