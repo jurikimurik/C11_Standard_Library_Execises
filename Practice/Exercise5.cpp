23753f5d14594b2d1d4b747d4ceb89c8e2d76819
jurikimurik <makovskyiyurii@gmail.com>
2023-02-18 22:25:45 +0100

    Added Exercise5 in Practice

---
+++ Practice/Exercise5.cpp
@@ -0,0 +1,2 @@
+// Zadanie 5 - Generator losowych liczb w zadanym przedziale. Niech uzytkwonik zgeneruje okreslone liczby losowe i bedzie mogl ich
+// posortowac, zapisac/odczytac, dodac w okreslone miejsce.
