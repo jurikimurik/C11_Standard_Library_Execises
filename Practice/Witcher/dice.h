b04303636e94d625e66f8de25bca4d46f8c8553e
jurikimurik <makovskyiyurii@gmail.com>
2023-02-24 19:11:11 +0100

    Created the main header files.

--- Practice/Witcher/dice.h
+++ Practice/Witcher/dice.h
@@ -1,3 +1,31 @@
+#include <deque>
+////////////////////////////////////////////////////////////////////////////
+enum class reprezentacja
+{
+    jeden = 1,
+    dwa,
+    trzy,
+    cztery,
+    piec,
+    szesc
+};
+////////////////////////////////////////////////////////////////////////////
 class Dice
 {
+    private:
+        reprezentacja rodzaj_kosci;
+
+        std::deque<char> visual;
+
+    public:
+        // Konstruktor domniemany z mozliwoscia losowosci
+        Dice(bool losowosc = false) {}
+
+        // Konstruktor z parametrem reprezentacji
+        Dice(reprezentacja rodzaj) : rodzaj_kosci(rodzaj) {}
+
+        // Konstruktor z parameterem typu int do reprezentacji
+        Dice(int kosc) : rodzaj_kosci(static_cast<reprezentacja>(kosc)) {}
+
+        std::string daj_wizualna_reprezentacje();
 };
--- Practice/Witcher/player.h
+++ Practice/Witcher/player.h
@@ -1,3 +1,38 @@
+#include <list>
+#include <string>
+#include "dice.h"
+////////////////////////////////////////////////////////////////////////////
 class Player
 {
+    private:
+        std::string nazwa_gracza;
+        bool czy_komputer;
+        std::list<Dice> koscie_gracza;
+
+    public:
+        // Konstruktor domniemany tworzy komputerowego gracza
+        Player() : czy_komputer(true), nazwa_gracza("Przeciwnik") {}
+
+        //Konstruktor sparametryzowany tworzy zwyklego gracza
+        Player(std::string imie, bool czy_k) : nazwa_gracza(imie), czy_komputer(czy_k) {}
+
+        auto daj_imie()
+        {
+            return nazwa_gracza;
+        }
+
+        void ustaw_imie(std::string imie)
+        {
+            nazwa_gracza = imie;
+        }
+
+        bool czy_jest_komputerem()
+        {
+            return czy_komputer;
+        }
+
+        auto daj_koscie()
+        {
+            return koscie_gracza;
+        }
 };
