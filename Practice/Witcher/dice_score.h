a1e376554844877a2df90369cf8232cc4d1e225f
jurikimurik <makovskyiyurii@gmail.com>
2023-02-26 21:07:07 +0100

    Created object that returns score.

---
+++ Practice/Witcher/dice_score.h
@@ -0,0 +1,9 @@
+#include "player.h"
+//********************************************************************************************************************
+class Dice_Score
+{
+    public:
+    Dice_Score() {}
+
+    int operator()(rodzaj_kontenera koscie);
+};
