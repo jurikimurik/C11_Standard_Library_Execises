38e821817b065b3938173fa228fd912d425a1488
jurikimurik <makovskyiyurii@gmail.com>
2023-02-26 21:52:51 +0100

    Almost finished dice_Score.

--- Practice/Witcher/dice_score.cpp
+++ Practice/Witcher/dice_score.cpp
@@ -1,26 +1,39 @@
 #include "dice_score.h"
 using namespace std;
-//************************************************************************************************
+//************************************************************************************************************************************
+rodzaj_wygranej Dice_Score::jaka_wygrana(rodzaj_kontenera koscie)
+{
+    
+}
+//************************************************************************************************************************************
 Dice_Score::Dice_Score(int generacja = 6)
 {
+    rodzaj_wygranej rodzaj = rodzaj_wygranej::nic;
+    int wygrana = 0;
+    auto pos = nazwy.begin();
 
+    for(const auto& elem : nazwy)
+    {
+        nagrody.insert(make_pair(rodzaj, wygrana));
+        nazwa_nagrody.insert(make_pair(rodzaj, *pos));
+
+        rodzaj = static_cast<rodzaj_wygranej>(static_cast<int>(rodzaj) + 1);
+        ++pos;
+        wygrana += 100;
+    }
 }
-//************************************************************************************************
+//************************************************************************************************************************************
 int Dice_Score::daj_wynik(rodzaj_kontenera koscie)
 {
-    auto pos = nagrody.find(koscie);
-    if(pos != nagrody.end())
-        return pos->second;
-    else
-        return 0;
+    rodzaj_wygranej jaki = jaka_wygrana(koscie);
+    auto pos = nagrody.find(jaki);
+    return pos->second;
 }
 
-//************************************************************************************************
+//************************************************************************************************************************************
 string Dice_Score::daj_nazwe_wyniku(rodzaj_kontenera koscie)
 {
-    auto pos = nazwa_nagrody.find(koscie);
-    if(pos != nagrody.end())
-        return pos->second;
-    else
-        return "Nic";
+    rodzaj_wygranej jaki = jaka_wygrana(koscie);
+    auto pos = nazwa_nagrody.find(jaki);
+    return pos->second;
 }
--- Practice/Witcher/dice_score.h
+++ Practice/Witcher/dice_score.h
@@ -1,16 +1,31 @@
 #include "player.h"
 #include "dice.h"
 #include <map>
+#include <vector>
+//********************************************************************************************************************
+vector<string> nazwy = {"Nic", "Swieta dwojka", "Potezna trojka", "Mnichowie", "Czworka dziewic", "Studenci", "Poker"};
 //********************************************************************************************************************
 class Dice_Score
 {
-    private:
-        std::map<rodzaj_kontenera, int> nagrody;
-        std::map<rodzaj_kontenera, string> nazwa_nagrody;
+    enum class rodzaj_wygranej
+    {
+        nic = 1,
+        dwa,
+        trzy,
+        cztery,
+        piec,
+        od_1_do_5,
+        od_2_do_6,
+    };
 
-    public:
-        Dice_Score(int generacja = 6);
+private:
+    std::map<rodzaj_wygranej, int> nagrody;
+    std::map<rodzaj_wygranej, string> nazwa_nagrody;
 
-        int daj_wynik(rodzaj_kontenera koscie);
-        std::string daj_nazwe_wyniku(rodzaj_kontenera koscie);
+public:
+    Dice_Score(int generacja = 5);
+
+    int daj_wynik(rodzaj_kontenera koscie);
+    std::string daj_nazwe_wyniku(rodzaj_kontenera koscie);
+    rodzaj_wygranej jaka_wygrana(rodzaj_konetenra koscie);
 };
