849a1c04c8efd448380c876ea5cf874c36c889f4
jurikimurik <makovskyiyurii@gmail.com>
2023-03-01 19:22:24 +0100

    Ex9, 10, 11 created.

---
+++ Section 11/Exercise9.cpp
@@ -0,0 +1,3 @@
+// Zadanie 9 - Magazyn skrzyn. Posiadasz magazyn skrzyn w ktorym kazda skrzynia jest reprezentowana liczbą losową. Pozwol uzytkownikowi
+// zliczyc interesujace go skrzynie za pomoca podania liczby, ktora musi byc wieksza niz interesujace go skrzynie. Niech takze bedzie w
+// stanie znalezc okreslony ciag skrzyn (liczb) na pozycjach (wypisz pozycje) lub ilosc powtarzajacych sie skrzyn wedlug numeru.
