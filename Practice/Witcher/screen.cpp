69c8f0206b1fdd0c1e85ae1654444d5d736514f0
jurikimurik <makovskyiyurii@gmail.com>
2023-03-03 07:27:29 +0100

    <- to previous commit

--- Practice/Witcher/screen.cpp
+++ Practice/Witcher/screen.cpp
@@ -16,7 +16,11 @@ void Screen::zaladuj_obiekty()
         {
             for (int x = coordinates.first; *wsk != '\n' && wsk != reprezentacja.end(); ++x, ++wsk)
             {
-                dane[y][x] = *wsk;
+                if(*wsk != ' ')
+                {
+                    dane[y][x] = *wsk;
+                }
+                
             }
             if(*wsk == '\n')
                 ++wsk;
@@ -46,4 +50,53 @@ Screen &Screen::operator-(const Text_Object &obj)
 {
     objekty.erase(remove(objekty.begin(), objekty.end(), &obj), objekty.end());
     return *this;
+}
+//************************************************************************************************************
+coordinates_t Screen::get_coordinate_position(position pozycja)
+{
+    resolution_t wymiary = get_resolution();
+    coordinates_t koordynaty = make_pair(0, 0);
+
+    switch (pozycja)
+    {
+    case position::lewa_gora:
+        koordynaty = make_pair(0, 0);
+        break;
+    case position::srodek_gora:
+        koordynaty = make_pair(wymiary.first / 2, 0);
+        break;
+    case position::prawa_gora:
+        koordynaty = make_pair(wymiary.first, 0);
+        break;
+
+    case position::lewa_srodek:
+        koordynaty = make_pair(0, wymiary.second / 2);
+        break;
+    case position::srodek:
+        koordynaty = make_pair(wymiary.first / 2, wymiary.second / 2);
+        break;
+    case position::prawa_srodek:
+        koordynaty = make_pair(wymiary.first, wymiary.second / 2);
+        break;
+
+    case position::lewa_dol:
+        koordynaty = make_pair(0, wymiary.second);
+        break;
+    case position::srodek_dol:
+        koordynaty = make_pair(wymiary.first / 2, wymiary.second);
+        break;
+    case position::prawy_dol:
+        koordynaty = make_pair(wymiary.first, wymiary.second);
+        break;
+
+    default:
+        break;
+        }
+
+    return koordynaty;
+}
+//************************************************************************************************************
+void tryb_wprowadzania(bool czy_wlaczyc, const Text_Object &obj = Text_Object())
+{
+    
 }
