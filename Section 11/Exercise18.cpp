58f8c846c037756dcf2e8708a99d0cdaf98c1b1e
jurikimurik <makovskyiyurii@gmail.com>
2023-03-07 21:53:04 +0100

    Completed Ex18.

--- Section 11/Exercise18.cpp
+++ Section 11/Exercise18.cpp
@@ -1,2 +1,24 @@
 // Zadanie 18 - Bez pierwszych! Wypełnij kolejkę deque liczbami kolejnymi zaczynajac od 10 i do 100. Następnie zmień każdą pierwsza 
 // liczbę na 0 i wypisz tą kolejkę.
+#include <deque>
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+using namespace std;
+//********************************************************************************************************************************
+int main()
+{
+    deque<int> kolejka;
+    kolejka.resize(100 - 10 + 1);
+    iota(kolejka.begin(), kolejka.end(), 10);
+
+    transform(kolejka.begin(), kolejka.end(), ostream_iterator<int>(cout, " "), [](int &liczba)
+                { 
+            for (int i = 2; i < liczba; ++i) {
+                if(liczba % i == 0)
+                    return liczba;
+            }
+
+            return 0; 
+            });
+}
