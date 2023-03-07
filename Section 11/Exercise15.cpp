24e279b7608ef905f861653f5030ce07232c6690
jurikimurik <makovskyiyurii@gmail.com>
2023-03-07 13:19:51 +0100

    Completed Ex15 + selfmade class for check. + Ex16(to previous commit)

--- Section 11/Exercise15.cpp
+++ Section 11/Exercise15.cpp
@@ -1,1 +1,69 @@
 // Zadanie 15 - Copy Vs. Move. Posiadasz 100 000 liczb losowych w wektorze. Porównaj szybkość kopiowania za pomocą metody copy oraz move.
+#include <vector>
+#include <chrono>
+#include <iostream>
+using namespace std;
+//******************************************************************************************************************************
+class SuperInteger
+{
+    int liczba;
+
+    int *ilosc_liczb;
+
+public:
+    SuperInteger(int l) : liczba(l) { ilosc_liczb = new int[liczba]; }
+
+    ~SuperInteger() { delete ilosc_liczb; }
+
+    SuperInteger(const SuperInteger& inny)
+    {
+        liczba = inny.liczba;
+        ilosc_liczb = new int[liczba];
+    }
+
+    SuperInteger(SuperInteger &&do_utylizacji) {
+        if(do_utylizacji == *this)
+            return;
+
+        ilosc_liczb = do_utylizacji.ilosc_liczb;
+        liczba = do_utylizacji.liczba;
+
+        do_utylizacji.ilosc_liczb = nullptr;
+    }
+
+    bool operator==(const SuperInteger& inny) const
+    {
+        return liczba == inny.liczba && ilosc_liczb == inny.ilosc_liczb;
+    }
+
+    
+};
+//******************************************************************************************************************************
+void wypelnij_wektor(vector<SuperInteger>& liczby, int ilosc_liczb = 500000)
+{
+    srand(time(NULL));
+
+    liczby.reserve(ilosc_liczb);
+
+    while(ilosc_liczb > 0)
+    {
+        liczby.push_back(rand() % ilosc_liczb);
+        ilosc_liczb--;
+    }
+}
+//******************************************************************************************************************************
+int main()
+{
+    vector<SuperInteger> liczby;
+    wypelnij_wektor(liczby);
+
+    vector<SuperInteger> pierwszy;
+    auto czas = chrono::system_clock::now();
+    copy(liczby.begin(), liczby.end(), back_inserter(pierwszy));
+    cout << "Czas potrzebny (copy): " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - czas).count() << " ms." << endl;
+
+    vector<SuperInteger> drugi;
+    czas = chrono::system_clock::now();
+    move(liczby.begin(), liczby.end(), back_inserter(drugi));
+    cout << "Czas potrzebny (move): " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - czas).count() << " ms." << endl;
+}
---
+++ Section 11/Exercise16.cpp
@@ -0,0 +1,2 @@
+// Zadanie 16 - Deszyfrator. Posiadasz słowo, w którym określone litery zostały zastapione innymi oraz zmieniła się ich kolejność. 
+//Poskładaj słowo z powrotem w jak najmniejszej ilości kroków.
