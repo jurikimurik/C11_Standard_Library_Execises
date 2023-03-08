ae271da8e46d6221852fe41a42a965df414e7a53
jurikimurik <makovskyiyurii@gmail.com>
2023-03-08 15:39:14 +0100

    Ex19-21 created.

---
+++ Section 11/Exercise20.cpp
@@ -0,0 +1,5 @@
+// Zadanie 20 - Turbo-huragan. Użytkownik podaje słowo, które w zależności od losowości zostaje zmienione w trzy sposoby: może się odwrócić, 
+// przemieścić się lub zostać spermutowane lub to wszystko na raz.
+//Reverse
+//Rotate
+//Permutation
---
+++ Section 11/Exercise21.cpp
@@ -0,0 +1,6 @@
+// Zadanie 21 - Szkolne kryteria. Zostaje generowany określony ciąg liczb od 1 do 5. Następnie część która zdaje (według której oceny się 
+// zdaje podaje użytkownik) zostaje zapisana w jednym zbiorze a druga część - w innym. Pokazana następnie jest ilość ludzi w jednym 
+// zbiorze i w drugim.
+//Shuffle
+//Partition
+//Partition copy
