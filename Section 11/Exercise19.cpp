0e2f9a5c490169522f6dc8cfaebe4c45b86e3b73
jurikimurik <makovskyiyurii@gmail.com>
2023-03-09 11:17:15 +0100

    <- deleted unnecessary comments

--- Section 11/Exercise19.cpp
+++ Section 11/Exercise19.cpp
@@ -1,8 +1,5 @@
 // Zadanie 19 - Program generuje użytkownikowi hasło i wypisuje go. Następnie użytkownik może z niego usunąć określoną literę lub od 
 // określona jego część. Również może zrobić tak, żeby elementy sąsiadujące ze sobą się nie powtarzały.
-//Remove
-//Remove copy
-//Unique
 #include <iostream>
 #include <algorithm>
 #include <iterator>
