39f8ca888cb386e0552938f4e12b096409acbfc3
jurikimurik <makovskyiyurii@gmail.com>
2023-03-09 11:23:22 +0100

    Completed Ex21.

--- Section 11/Exercise21.cpp
+++ Section 11/Exercise21.cpp
@@ -4,3 +4,39 @@
 //Shuffle
 //Partition
 //Partition copy
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+vector<int> generuj_ciag()
+{
+    srand(time(NULL));
+    vector<int> ciag;
+
+    for (int ilosc = 0; ilosc < 90; ++ilosc)
+    {
+        ciag.push_back(rand() % 5 + 1);
+    }
+
+    return ciag;
+}
+
+int main()
+{
+    vector<int> ciag = generuj_ciag();
+
+    cout << "Podaj ocene przy ktorej sie zdaje: ";
+    int ocena;
+    cin >> ocena;
+
+    vector<int> dobry_ciag;
+    vector<int> zly_ciag;
+
+    partition_copy(ciag.begin(), ciag.end(), back_inserter(dobry_ciag), back_inserter(zly_ciag), [&ocena](int &liczba)
+                   { return liczba >= ocena; });
+
+    cout << "Ilosc porzadnych uczniow: " << dobry_ciag.size() << endl;
+
+    cout << "Ilosc leniwcow: " << zly_ciag.size() << endl;
+}
