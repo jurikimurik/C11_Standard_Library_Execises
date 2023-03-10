d35561ec434341c2014553f699bdc0fe43c3d866
jurikimurik <makovskyiyurii@gmail.com>
2023-03-10 18:15:31 +0100

    <- to previous commit

---
+++ Section 11/Exercise25.cpp
@@ -0,0 +1,2 @@
+// Zadanie 25 - Poszukiwacz. Jest ciąg liczb generowanych losowo. Następnie jest posortowany. Niech użytkownik wprowadzi ciąg i sprawdzi 
+// czy zadany ciąg jest w nim.
---
+++ Section 11/Exercise27.cpp
@@ -0,0 +1,2 @@
+// Zadanie 27 - Ultra-poszukiwacz. Niech użytkownik wprowadzi dwa ciągi. Posortuj je. Pozwól mu sprawdzić wspólne elementy, połączone 
+// elementy, różnice i różnice symetryczna.
