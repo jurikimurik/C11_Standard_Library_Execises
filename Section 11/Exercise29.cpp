5e71a25927ec6fe04ee1d62345460790ac4091cc
jurikimurik <makovskyiyurii@gmail.com>
2023-03-11 06:57:54 +0100

    Exercise29 is done.

--- Section 11/Exercise29.cpp
+++ Section 11/Exercise29.cpp
@@ -1,2 +1,16 @@
-// Zadanie 29 - Iloczyn skalarny. Zostaje generowany ciąg licz i wypisany na ekranie. Następnie należy obliczyć jego sumę skalarna i 
+// Zadanie 29 - Iloczyn skalarny. Zostaje generowany ciąg liczb i wypisany na ekranie. Następnie należy obliczyć jego sumę skalarna i 
 // wypisać na ekranie.
+#include "narzedzia.h"
+#include <numeric>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    auto ciag = generuj_ciag_liczbowy<vector<int>>(0, 100, 20);
+
+    drukuj(ciag, "", "\n");
+
+    cout << "Iloczyn skalarny: " << inner_product(ciag.begin(), ciag.end(), ciag.begin(), 0);
+    cout << endl;
+}
