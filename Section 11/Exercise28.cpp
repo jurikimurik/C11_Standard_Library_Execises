c66af8faf1445437d2194e671a8f069cd5dbdd00
jurikimurik <makovskyiyurii@gmail.com>
2023-03-11 06:50:10 +0100

    Exercises28-30 created.

---
+++ Section 11/Exercise28.cpp
@@ -0,0 +1,2 @@
+// Zadanie 28 - Liczymy ciąg. Użytkownik podaje ciąg. Następnie wybiera czy mamy dodać do siebie wszystkie jego elementy czy odjąć i 
+// wypisuje rezultat na ekranie.
---
+++ Section 11/Exercise29.cpp
@@ -0,0 +1,2 @@
+// Zadanie 29 - Iloczyn skalarny. Zostaje generowany ciąg licz i wypisany na ekranie. Następnie należy obliczyć jego sumę skalarna i 
+// wypisać na ekranie.
