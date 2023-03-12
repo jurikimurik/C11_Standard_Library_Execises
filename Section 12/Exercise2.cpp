9bd5da72d200bacd900eb30a0081bf8800f95e30
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-12 09:46:27 +0100

    Exercise2 is done.

--- Section 12/Exercise2.cpp
+++ Section 12/Exercise2.cpp
@@ -1,2 +1,43 @@
 // Zadanie 2 - Kolejka w Biedronce. Stworz klase "Klient" z imionem i nazwiskiem. Stworz kilka takich klientow i umieszcz ich w kolejce do
 // kasy. Nastepnie niech pani Kasia obsluzy ich wszystkich po kolei i wypisze na ekranie.
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Klient
+{
+private:
+    string imie;
+
+public:
+    Klient(string i) : imie(i) {}
+
+    string get_imie() const
+    {
+        return imie;
+    }
+};
+
+ostream &operator<<(ostream &ekran, const Klient &k)
+{
+    ekran << k.get_imie();
+    return ekran;
+}
+
+int main()
+{
+    Klient Stachu("Staszek");
+    Klient Maks("Maks");
+    Klient Evgeniy("Evgeniy");
+
+    queue<Klient> kasa;
+    kasa.push(Maks);
+    kasa.push(Stachu);
+    kasa.push(Evgeniy);
+
+    while (!kasa.empty())
+    {
+        cout << "Klient " << kasa.front() << " zostal obsluzony przez kasjerke pania Kasie!" << endl;
+        kasa.pop();
+    }
+}
