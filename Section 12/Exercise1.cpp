2a8f09c578855d640a344ae6c9ed47beb9e106ec
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-12 09:46:00 +0100

    Exercise1 is done.

--- Section 12/Exercise1.cpp
+++ Section 12/Exercise1.cpp
@@ -1,1 +1,22 @@
 // Zadanie 1 - Stos ksiazek. Posiadasz zestaw ksiazek. Wrzuc ich na stos a nastepnie odczytaj po kolei i wypisz na ekranie.
+#include <stack>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    vector<string> ksiazki = {"Pan Lodowego Ogrodu", "Sztywny", "Hobbit", "Miecz przeznaczenia", "Ostatnie zyczenie"};
+
+    stack<string> stos_ksiazek;
+    for(auto& elem : ksiazki)
+    {
+        stos_ksiazek.push(move(elem));
+    }
+
+    while(!stos_ksiazek.empty())
+    {
+        cout << stos_ksiazek.top() << ", ";
+        stos_ksiazek.pop();
+    }
+}
