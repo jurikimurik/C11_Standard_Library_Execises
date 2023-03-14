23e2330b0da684b9e243fb417cfaea67ebecfc27
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-14 20:57:46 +0100

    Exercise 6 is completed.

--- Section 12/Exercise6.cpp
+++ Section 12/Exercise6.cpp
@@ -1,2 +1,38 @@
 // Zadanie 6 - Rózny rodzaj liczb. Niech użytkownik najpierw wybierze w jakim trybie wprowadza liczbę (01/8/10/16). Następnie program 
 // przerobi to na liczbę zwykła i wypisze na ekranie.
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include "narzedzia.h"
+using namespace std;
+
+int main()
+{
+    int podstawa = 0;
+    while (podstawa == 0)
+    {
+        int rodzaj = wprowadzenie<int>("Rodzaj liczby: 1 - zero-jedynkowa, 2 - osemkowa, 3 - dziesietna, 4 - szesnastkowa: ");
+
+        switch (rodzaj)
+        {
+        case 1:
+            podstawa = 2;
+            break;
+        case 2:
+            podstawa = 8;
+            break;
+        case 3:
+            podstawa = 10;
+            break;
+        case 4:
+            podstawa = 16;
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    string liczba = wprowadzenie<string>("Wprowadz liczbe: ");
+    cout << "Twoja liczba przy podstawie " << podstawa << " - to: " << stoll(liczba, nullptr, podstawa) << endl;
+}
