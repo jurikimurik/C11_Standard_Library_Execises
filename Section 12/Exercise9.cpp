9df6557a8610947e7bc3f66060285cfc2fbeab7c
Yurii <makovskyiyurii@gmail.com>
2023-03-15 21:44:13 +0100

    Completed Exercise9 in Section12.

--- Section 12/Exercise9.cpp
+++ Section 12/Exercise9.cpp
@@ -1,2 +1,32 @@
 // Zadanie 9 - Cenzura. Uzytkownik podaje fraze, ktora nastepnie jest cenzurowana w ten sposob, ze okreslone slowa zostaja zastapione 
 // przez inne slowa.
+
+#include "narzedzia.h"
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string rozd {" ,.;!?"};
+   
+    map<string, string> cenzura = {make_pair("wolnosc", "ograniczenie"), make_pair("lepszych", "dostosowanych do potrzeb"), make_pair("godnego", "rownego")};
+
+    string fraza = wprowadzenie<string>("Wprowadz fraze: ");
+
+    for(const auto& elem : cenzura) {
+       string::iterator pos = search(fraza.begin(), fraza.end(), elem.first.begin(), elem.first.end());
+
+        
+     if (pos != elem.first.end())
+     {
+        string::iterator pos2 = find_first_of(pos, fraza.end(), rozd.begin(), rozd.end());
+        fraza.replace(pos, pos2, elem.second);
+        
+     }
+
+    }
+
+    cout << fraza << endl;
+}
