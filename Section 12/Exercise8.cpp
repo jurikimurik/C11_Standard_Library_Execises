a5caf12068fb0b666aa3a0ac863cec47b797ce4c
Yurii <makovskyiyurii@gmail.com>
2023-03-15 21:37:44 +0100

    Exercise 8 are completed.

--- Section 12/Exercise8.cpp
+++ Section 12/Exercise8.cpp
@@ -1,2 +1,34 @@
 // Zadanie 8 - Fala. Uzytkownik poda slowo, z ktorym nastepnie program zrobi krotka animacje typu fala (litery sie powiekszaja i 
 // zmniejszaja).
+
+#include "narzedzia.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+int main()
+{
+    string slowo = wprowadzenie<string>("Wprowadz slowo:");
+
+    auto odstep = chrono::milliseconds(50);
+    string::iterator wsk = slowo.begin();
+    do {
+        *wsk = toupper(*wsk);
+        cout << slowo << '\r' << flush;
+
+        
+        this_thread::sleep_for(odstep);
+        wsk++;
+    } while (wsk != slowo.end());
+
+    wsk = slowo.begin();
+    do {
+        *wsk = tolower(*wsk);
+        cout << slowo << '\r' << flush;
+
+        
+        this_thread::sleep_for(odstep);
+        wsk++;
+    } while (wsk != slowo.end());
+}
