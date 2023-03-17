c820865de85dcbdcb0827ea73d203197af8b8bc8
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-17 15:24:08 +0100

    Completed Ex7 in Section 14.

--- Section 14/Exercise7.cpp
+++ Section 14/Exercise7.cpp
@@ -1,2 +1,22 @@
 // Zadanie 7 - Sprawdzasz błędów. Pozwól użytkownikowi samemu stworzyć wyrażenie regularne. Następnie niech program sprawdzi czy jest to
 // dobre wyrażenie. Niech wypisze informacje na ekranie.
+#include <regex>
+#include <iostream>
+#include "../narzedzia.h"
+using namespace std;
+
+int main()
+{
+    string str = wprowadzenie<string>("Wprowadz wyrazenie regularne w jezyku ECMA Script: ");
+
+    try {
+        regex reg(str);
+
+        cout << "Dobry kod!" << endl;
+    }
+    catch (const regex_error &e)
+    {
+        cout << "Zly kod!" << endl;
+        cerr << e.what() << endl;
+    }
+}
