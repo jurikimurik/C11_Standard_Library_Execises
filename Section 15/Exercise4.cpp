4d3db6eba828ff5a923bd40f25c87199cd943c95
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-18 14:13:26 +0100

    Exercise4 has been completed in Section 15.

--- Section 15/Exercise4.cpp
+++ Section 15/Exercise4.cpp
@@ -1,2 +1,26 @@
 // Zadanie 4 - Strumienie plikowe.Niech strumien wczytuje plik w którym są same liczbę i jedna litera.Ustaw go tak, by podczas błędu 
 // rzucał wyjątek.
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream strm("liczby.data");
+    strm.exceptions(ios_base::failbit);
+
+    try
+    {
+        while(strm)
+        {
+            cout << "Ladujemy liczbe..." << endl;
+            string strm_napis;
+            int liczba;
+            strm >> liczba;
+        }
+    }
+    catch (...)
+    {
+        cout << "Zadzialalo! Brawo!";
+    }
+}
