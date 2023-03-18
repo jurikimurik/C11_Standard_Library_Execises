1492d5711b87adf0feed3aeb8e83c0ae05fcf8fd
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-18 14:09:46 +0100

    Completed Exercise3 in section 15.

--- Section 15/Exercise3.cpp
+++ Section 15/Exercise3.cpp
@@ -1,1 +1,20 @@
 // Zadanie 3 - W powietrze! Ustaw strumien w taki tryb, by wyrzucał wyjątki, jeżeli coś nie będzie mu odpowiadać. Przetestuj to.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cin.exceptions(ios_base::failbit);
+
+    try {
+        cout << "Wprowadz zla liczbe: " << endl;
+        int liczba;
+        cin >> liczba;
+
+        cout << "Nie zadzialalo :(";
+    }
+    catch (...)
+    {
+        cout << "Zadzialalo! Brawo!";
+    }
+}
