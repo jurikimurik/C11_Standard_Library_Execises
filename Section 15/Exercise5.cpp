a395d6f4296a907350b04fa3cc50c627951731d3
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-19 16:08:58 +0100

    Exercise5 is complete now.

--- Section 15/Exercise5.cpp
+++ Section 15/Exercise5.cpp
@@ -1,2 +1,17 @@
 // Zadanie 5 - Polskie znaczki tez się liczą! Za pomocą podstawowych funkcji pobierających, pobierz tekst z polskimi znaczkami a 
 // następnie wypisz go na ekranie.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    wchar_t znaczek;
+    wstring poslkie_slowo;
+
+    while(wcin.get(znaczek))
+    {
+        poslkie_slowo += znaczek;
+    }
+
+    wcout << "Twoje slowo: " << poslkie_slowo << endl;
+}
