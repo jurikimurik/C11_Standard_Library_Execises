74120bce8aadb4131c0c72122157b60c78058a15
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-20 12:10:48 +0100

    Exercise8 in Sec. 15 is now complete.

--- Section 15/Exercise8.cpp
+++ Section 15/Exercise8.cpp
@@ -1,2 +1,29 @@
 // Zadanie 8 - Spis gości. Posiadasz plik goscie.data. Odczytaj z niego dane do strumienia cout za pomocą funkcji get oraz zwykłych 
 // przeciążonych funkcji operatora << dla strumieni.
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream plik("goscie.data");
+
+    if(!plik) {
+        cout << "Nie udalo sie zaladowac. Abort!" << endl;
+        return 0;
+    }
+
+    char znak;
+    while(plik.get(znak))
+    {
+        cout.put(znak);
+    }
+
+    plik.clear();
+    plik.seekg(0);
+
+    cout << endl
+         << endl;
+
+    cout << plik.rdbuf() << endl;
+}
