0302d4c40047cde906bfd722b40c63b2b2d8f9d7
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-20 12:33:51 +0100

    Ex.10 in Section 15 is now complete.

--- Section 15/Exercise10.cpp
+++ Section 15/Exercise10.cpp
@@ -1,2 +1,29 @@
 // Zadanie 10 - Posiadasz folder o nazwie „wierszy”. Niech program odczyta każdy z tych wierszy i wypisze ich na ekranie, 
 // rozdzielając liniami nowego wiersza.
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ifstream strm;
+
+    int index = 1;
+    while (true)
+    {
+        strm.open("wierszy/wiersz" + to_string(index) + ".w");
+        if (!strm)
+        {
+            cout << "\n\tBRAK WIERSZY!" << endl;
+            break;
+        }
+
+        cout << "Wierszyk numer " << index << " !" << endl
+             << endl;
+
+        cout << strm.rdbuf() << endl
+             << endl;
+        index++;
+        strm.close();
+    }
+}
