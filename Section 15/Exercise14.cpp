bc99769f8f69a0af2ebd09e431e06a8292c86b6c
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-21 20:54:54 +0100

    Exercise 14 in Section 15 is now complete.

--- Section 15/Exercise14.cpp
+++ Section 15/Exercise14.cpp
@@ -1,3 +1,28 @@
-// Zadanie 14 - Pomocniki. Stwórz dwa dodatkowe strumienie związane ze strumieniem cout, tylko ze będą liczby wypisywać w postaci szesnastkowej oraz ósemkowej. Niech użytkownik wprowadzi swoje liczby, które następnie trzeba będzie pokazać w dwóch innych dodatkowych postaciach użytkownikowi.
+// Zadanie 14 - Pomocniki. Stwórz dwa dodatkowe strumienie związane ze strumieniem cout, tylko ze będą liczby wypisywać w postaci szesnastkowej oraz ósemkowej. 
+// Niech użytkownik wprowadzi swoje liczby, które następnie trzeba będzie pokazać w dwóch innych dodatkowych postaciach użytkownikowi.
 //Streambuffer
 //Streamredirect
+#include <iostream>
+#include <vector>
+#include "../narzedzia.h"
+using namespace std;
+
+int main()
+{
+    ostream strm_hex(cout.rdbuf());
+    strm_hex << hex;
+
+    ostream strm_oct(cout.rdbuf());
+    strm_oct << oct;
+
+    vector<int> liczby = wprowadz_ciag<vector<int>>("Wprowadzaj liczby rozdzielajac ich spacjami a nastepnie wpisz 'q': ");
+
+    for(const auto& elem : liczby)
+    {
+        strm_oct << elem << " ";
+        cout << elem << " ";
+        strm_hex << elem << " ";
+
+        cout << endl;
+    }
+}
