51f99aab09d88495e2f4f1389171cc5e27ed8ad9
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-21 21:13:16 +0100

    Exercise 15 in Section 15 is complete.

--- Section 15/Exercise15.cpp
+++ Section 15/Exercise15.cpp
@@ -1,2 +1,39 @@
 // Zadanie 15 - Użytkownik wprowadza swoje zdanie. Następnie każde słowo rozdzielone spacjami trzeba zamienić na inne słowa, ktore użytkownik będzie chciał. Po wszystkim należy wypisać otrzymane słowo.
-//Streamreadwrite
+#include <iostream>
+#include <sstream>
+#include "../narzedzia.h"
+using namespace std;
+
+int main()
+{
+    string zdanie = wprowadzenie<string>("Wprowadz zdanie: ");
+
+    stringstream strm(zdanie);
+    strm.seekg(0);
+
+    while(strm)
+    {
+        strm >> ws;
+
+        // Ustawiwamy na dobrym miejscu
+        strm.seekp(strm.tellg());
+
+        string slowo;
+        strm >> slowo;
+        if(!strm)
+            break;
+
+        string zamiana = wprowadzenie<string>("Wprowadz na co mam zmienic \"" + slowo + "\":");
+        if(zamiana.length() < slowo.length())
+        {
+            cout << "JEST";
+            while (zamiana.length() != slowo.length())
+            {
+                zamiana += '\0';
+            }
+        }
+        strm << zamiana;
+    }
+
+    cout << strm.str();
+}
