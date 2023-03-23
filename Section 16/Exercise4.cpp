5f1f6002f91045a08509f1be61a764a0698a0fb2
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-23 14:57:00 +0100

    Ex4 in Section 16 is ready.

--- Section 16/Exercise4.cpp
+++ Section 16/Exercise4.cpp
@@ -1,2 +1,39 @@
-// Zadanie 4 - Cyber-money. Stwórz klasę cyebrpunktMoney, która zmieni aspekt wypisywania wartości pieniężnych w taki sposób, 
+// Zadanie 4 - Cyber-money. Stwórz klasę cyebrpunktMoney, która zmieni aspekt wypisywania wartości pieniężnych w taki sposób,
 // żeby przypominało wartości pieniężne z Cyberpunk. Następnie korzystając z tego wypisz jakakolwiek kwotę pieniężną.
+#include <locale>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+class cyberpunkMoney : public moneypunct_byname<char>
+{
+public:
+    cyberpunkMoney(const string &name) : moneypunct_byname<char>(name) {}
+
+private:
+    using typ_c = cyberpunkMoney::char_type;
+    using typ_s = cyberpunkMoney::string_type;
+
+protected:
+    virtual typ_c do_decimal_point() const
+    {
+        return '\0';
+    }
+    virtual typ_c do_thousands_sep() const
+    {
+        return '\0';
+    }
+    virtual typ_s do_curr_symbol() const
+    {
+        return "€$";
+    }
+};
+
+int main()
+{
+    long double pieniadze = 1500750;
+    cout.imbue(locale(locale("pl_PL"), new cyberpunkMoney("pl_PL")));
+
+    cout << showbase << fixed;
+    cout << "Pieniadze jak w Cyberpunk'u: " << put_money(pieniadze) << endl;
+}
