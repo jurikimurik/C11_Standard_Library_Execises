7c44c1f7786e5aaed625c8c25fe7356e61b1e7d1
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-23 14:35:33 +0100

    Ex3 in Section16 is now complete.

--- Section 16/Exercise3.cpp
+++ Section 16/Exercise3.cpp
@@ -1,2 +1,43 @@
 // Zadanie 3 - Głupia lokalizacja. Stwórz klasę glupiaLokalizacja, która zmieni aspekt wypisywania liczb w dziwny sposób. Następnie 
 // skorzystaj z tego aspektu, żeby wypisać jednak zwykła liczbę wielka i jedna zmiennoprzecinkowa.
+#include <locale>
+#include <iostream>
+using namespace std;
+
+class glupiaLokalizacja : public numpunct_byname<char> {
+    public:
+    glupiaLokalizacja(const string& name) : numpunct_byname<char>(name) {}
+
+    protected:
+    virtual string do_truename() const {
+        return "elo, gosciu";
+    }
+
+    virtual string do_falsename() const {
+        return "haha glupek";
+    }
+
+    virtual glupiaLokalizacja::char_type do_decimal_point() const {
+        return '+';
+    }
+
+    virtual glupiaLokalizacja::char_type do_thousands_sep() const {
+        return '|';
+    }
+
+    virtual glupiaLokalizacja::string_type do_grouping() const {
+        return {2, 0};
+    }
+};
+
+int main()
+{
+    long double liczba_zwykla = 124242.23;
+    long double liczba_niezwykla = 12.42342;
+
+    cout.imbue(locale(locale(""), new glupiaLokalizacja("")));
+
+    cout << "Wypisuje liczbe zwykla: " << liczba_zwykla << endl;
+    cout << "Wypisuje liczbe niezwykla: " << liczba_niezwykla << endl;
+    cout << "Sprawdzamy true i false: " << boolalpha << true << "/" << false << endl;
+}
