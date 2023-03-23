89c086a8616f7f1081546688b44a5567dfef068d
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-23 15:00:37 +0100

    Ex5 in Section 16 is complete.

--- Section 16/Exercise5.cpp
+++ Section 16/Exercise5.cpp
@@ -1,2 +1,23 @@
 // Zadanie 5 - Konwertor. Niech użytkownik poda ilość pieniędzy w złotówkach. Następnie ta liczbę należy przekonwertować 
 // na euro i wypisać zgodnie z formatem miedzynarodowym Niemieckim.
+#include <locale>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double wsp = 0.21;
+
+    cout.imbue(locale("pl_PL"));
+    cin.imbue(locale("pl_PL"));
+
+    cout << showbase << fixed;
+
+    long double pieniadze;
+    cout << "Wprowadz ilosc pieniedzy w zlotowkach: ";
+    cin >> get_money(pieniadze);
+
+    cout.imbue(locale("de_DE"));
+    cout << "Ilosc pieniedzy w euro: " << put_money(pieniadze * wsp, true) << endl;
+}
