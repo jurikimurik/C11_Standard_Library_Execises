d360eeaec07a3c18fbf072755c0cb247cb7caee8
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-24 17:51:19 +0100

    Ex9 in Section 16 is now completed.

--- Section 16/Exercise9.cpp
+++ Section 16/Exercise9.cpp
@@ -1,1 +1,21 @@
 // Zadanie 9 - Kodowanie i zapis UTF-16. Stwórz strumien z kodowaniem utf16, który zapisze tekst podany przez użytkownika do pliku utf16.txt
+#include <string>
+#include <iostream>
+#include <codecvt>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+
+    wofstream uft16outFile("utf16.data");
+    uft16outFile.imbue(locale(uft16outFile.getloc(), new std::codecvt_utf16<wchar_t, 0x10ffff, std::little_endian>));
+
+    wcout << "Wprowadz polskie nazwisko z dziwnym znaczkiem: ";
+    wstring nazwisko;
+    wcin >> nazwisko;
+    wcout << nazwisko << endl;
+
+    uft16outFile << nazwisko << endl;
+}
