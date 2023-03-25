1a276631e18575e8b992196de4cc7a388ba97e2f
Jurikimurik <makovskyiyurii@gmail.com>
2023-03-25 13:29:32 +0100

    Exercise1 in Section 17 is now complete.

--- Section 17/Exercise1.cpp
+++ Section 17/Exercise1.cpp
@@ -1,2 +1,30 @@
 // Zadanie 1 - Generacja liczb zmiennoprzecinkowych o zadanej precyzji. Niech użytkownik poda zakres, ilość liczb oraz 
-//precyzję generowanych liczb zmiennoprzecinkowych. Wygeneruj mu te liczby zmiennoprzecinkowe i wypisz ich na ekranie.
+// precyzję generowanych liczb zmiennoprzecinkowych. Wygeneruj mu te liczby zmiennoprzecinkowe i wypisz ich na ekranie.
+#include <iostream>
+#include <random>
+#include <cmath>
+#include <iomanip>
+#include "../narzedzia.h"
+using namespace std;
+
+int main()
+{
+    size_t ilosc = wprowadzenie<size_t>("Wprowadz ilosc liczb: ");
+
+    double zakres_od = wprowadzenie<double>("Wprowadz dolny zakes liczb: ");
+    double zakres_do = wprowadzenie<double>("Wprowadz gorny zakres liczb: ");
+
+    size_t precyzja = wprowadzenie<size_t>("Podaj prezycje liczb: ");
+
+    default_random_engine dre(time(nullptr));
+    uniform_real_distribution urd(zakres_od, zakres_do);
+
+    cout << fixed << setprecision(precyzja);
+    for (int i = 0; i < ilosc; ++i)
+    {
+        double liczba = urd(dre);
+        // opcjonalne obcinanie niepotrzebnych liczb przy precyzji
+        liczba = ceil(liczba*pow(10, precyzja))/pow(10,precyzja);
+        cout << liczba << " ";
+    }
+}
